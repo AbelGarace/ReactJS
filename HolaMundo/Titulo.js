7a9e1d85d4ce5cbf1307f026978a220a966c38a4
AbelGarace <abel.garace@gmail.com>
2017-02-09 21:04:38 +0100

    Hola Mundo 2

---
+++ HolaMundo/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Titulo = React.createClass({
+	render(){
+		return ( 
+				<div>
+				<h4>Hola Mundo</h4>
+				</div>
+			)
+	}
+});
+
+export default Titulo;
---
+++ HolaMundo/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import Titulo from './Titulo.js';
+
+ReactDOM.render(<Titulo/>, document.getElementById('titulo'));
+ReactDOM.render(<App />, document.getElementById('app'));
